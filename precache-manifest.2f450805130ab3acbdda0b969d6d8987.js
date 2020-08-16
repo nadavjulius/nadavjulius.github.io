d1f0082158e0f08ab95909faf1793edb9363fa6a
Nadav Julius <nadavjulius@gmail.com>
2020-08-17 00:05:27 +0300

    second commit

---
+++ precache-manifest.2f450805130ab3acbdda0b969d6d8987.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815bf5f5fde32b767a29befbee8cdc1c",
+    "url": "/Thumbs.db"
+  },
+  {
+    "revision": "578a5fc245b833772193",
+    "url": "/css/app.cc091485.css"
+  },
+  {
+    "revision": "1ac8ffd95c0705a5fb905ceccaa8514b",
+    "url": "/cv.pdf"
+  },
+  {
+    "revision": "f33327cdc7c1c759c638ac5860736cbd",
+    "url": "/fonts/CutiveMono-Regular.f33327cd.ttf"
+  },
+  {
+    "revision": "55421b062699870a1426fa1206b2acab",
+    "url": "/fonts/Inconsolata-Bold.55421b06.ttf"
+  },
+  {
+    "revision": "9d52a8c4fafc763d9a356d8b67d4a2e6",
+    "url": "/fonts/Inconsolata-Regular.9d52a8c4.ttf"
+  },
+  {
+    "revision": "c04d1551740d27ebcf504e8c7e12ed75",
+    "url": "/fonts/MajorMonoDisplay-Regular.c04d1551.ttf"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/fonts/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "7c13b04382bb3c4a6a50211300a1b072",
+    "url": "/fonts/RobotoMono-Bold.7c13b043.ttf"
+  },
+  {
+    "revision": "b4618f1f7f4cee0ac09873fcc5a966f9",
+    "url": "/fonts/RobotoMono-Regular.b4618f1f.ttf"
+  },
+  {
+    "revision": "55b4b33724cbc951645d220df1803f94",
+    "url": "/img/angular.55b4b337.png"
+  },
+  {
+    "revision": "a19bf947fb94f124c06bf4d4758847fc",
+    "url": "/img/appsus-prev.a19bf947.png"
+  },
+  {
+    "revision": "f175e13d0b8f32f473a6ce29dfef11d8",
+    "url": "/img/appsus.f175e13d.jpg"
+  },
+  {
+    "revision": "b9bea4248fa886016e98f2d822a1ad3d",
+    "url": "/img/basketball.b9bea424.png"
+  },
+  {
+    "revision": "6014410cbd6baab7d062c34e387e6389",
+    "url": "/img/caveLeft.6014410c.png"
+  },
+  {
+    "revision": "3d612663594a2c6af5e05d88a35e3236",
+    "url": "/img/caveRight.3d612663.png"
+  },
+  {
+    "revision": "f24949eda6c74fef495d240d79b91719",
+    "url": "/img/cloud.f24949ed.png"
+  },
+  {
+    "revision": "10b94ae120ca20fd39d998b8c59d94f2",
+    "url": "/img/dirtbgb.10b94ae1.jpg"
+  },
+  {
+    "revision": "f68d0995478790a0f9fdaec952f9b311",
+    "url": "/img/einstein.f68d0995.png"
+  },
+  {
+    "revision": "eca953658422204bc02afc99bf8598dc",
+    "url": "/img/github.eca95365.png"
+  },
+  {
+    "revision": "d825df9657135ea39077a1ddfc5b6dc4",
+    "url": "/img/gram.d825df96.png"
+  },
+  {
+    "revision": "f3118cb24716371164177745a01cd28c",
+    "url": "/img/linkedin.f3118cb2.png"
+  },
+  {
+    "revision": "72e166d1e432323b9cbe920c2a901eee",
+    "url": "/img/logoWhite.72e166d1.png"
+  },
+  {
+    "revision": "3be87e953f5452469bbc9c7a4d1b1627",
+    "url": "/img/mrbitcoinangular.3be87e95.jpg"
+  },
+  {
+    "revision": "da6d450fde48fdb1aadc340fff2fc3fb",
+    "url": "/img/mrbitcoinreact.da6d450f.jpg"
+  },
+  {
+    "revision": "94ed4fb4ea55f8ab4e7ac62dfc453e02",
+    "url": "/img/mrbtcangular-prev.94ed4fb4.png"
+  },
+  {
+    "revision": "4e5c4be84f3f0e40de9d857f6ad8244a",
+    "url": "/img/mrbtcreact-prev.4e5c4be8.png"
+  },
+  {
+    "revision": "86240e8cd9e15b717f6660d13c02c275",
+    "url": "/img/music.86240e8c.png"
+  },
+  {
+    "revision": "db9aceee8c045eada0f86f55658886ca",
+    "url": "/img/pics.db9aceee.png"
+  },
+  {
+    "revision": "6c71d323d5de307d42a7a389ec88f4a9",
+    "url": "/img/portfolio-prev.6c71d323.png"
+  },
+  {
+    "revision": "d1cb3af1e22736e3f2fe73be5fa8c9e3",
+    "url": "/img/react.d1cb3af1.png"
+  },
+  {
+    "revision": "bc4be80f87be25c64cc14e5c5d68d766",
+    "url": "/img/runGameBg.bc4be80f.png"
+  },
+  {
+    "revision": "2db60f077b3eca4aecb9d06dafb27f51",
+    "url": "/img/sass.2db60f07.png"
+  },
+  {
+    "revision": "a32b14cb9ee4deb8a42e8b298a121ef9",
+    "url": "/img/shellb.a32b14cb.gif"
+  },
+  {
+    "revision": "8fa3ebe2539e56b3e68b1cd8ad0aa029",
+    "url": "/img/shellg.8fa3ebe2.gif"
+  },
+  {
+    "revision": "6a6a7735da273ce920406561ac51b8dd",
+    "url": "/img/shellr.6a6a7735.gif"
+  },
+  {
+    "revision": "7691a49b7c8be3a23e994ea17ca3582b",
+    "url": "/img/sonic1.7691a49b.gif"
+  },
+  {
+    "revision": "1134f33805aa0ace99b2569a66353de1",
+    "url": "/img/sonic2.1134f338.gif"
+  },
+  {
+    "revision": "6676590ba747f17e2fa5ee5a0d3efa1b",
+    "url": "/img/sonic3.6676590b.gif"
+  },
+  {
+    "revision": "aee1b0bb1f151adffd7ccdeccf7a11eb",
+    "url": "/img/stack.aee1b0bb.png"
+  },
+  {
+    "revision": "64487f0590db0d3edccceccb2975d4c7",
+    "url": "/img/stackwhite.64487f05.png"
+  },
+  {
+    "revision": "a978d416c9579bf63609215ad28fd8d9",
+    "url": "/img/travel.a978d416.png"
+  },
+  {
+    "revision": "48874e567aa5ec1f539119cc00ed91c4",
+    "url": "/img/trollface.48874e56.png"
+  },
+  {
+    "revision": "3a218c827e21e1ae7d7b9128cde505d5",
+    "url": "/img/waveOn.3a218c82.jpg"
+  },
+  {
+    "revision": "efcde12dec253b554823206c175544b8",
+    "url": "/img/wo-prev.efcde12d.png"
+  },
+  {
+    "revision": "f89ee3a7369ff9f89c1d7ef21d5ebf75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "578a5fc245b833772193",
+    "url": "/js/app.5764aa94.js"
+  },
+  {
+    "revision": "910603065bcd5b2d82ad",
+    "url": "/js/chunk-vendors.b228a58a.js"
+  },
+  {
+    "revision": "ece55653a60946b1e6a1d3a1f49545ef",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
